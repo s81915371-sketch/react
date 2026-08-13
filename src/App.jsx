c0b04f5063356769f563fd0a38dcdb74e2d84d7d
Shamsiddin <s81915371@gmail.com>
2026-08-13 18:19:16 +0500

    react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { WiAlien } from "react-icons/wi";
+import { ImBug } from "react-icons/im";
+
+
+function App() {
+  return (
+  <div>
+<WiAlien />
+<ImBug />
+  </div>
+     )
+     
+}
+export default App
